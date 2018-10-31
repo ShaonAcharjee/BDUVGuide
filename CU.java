37b836c5bd1f762eb68951d732f6bf7e61271d66
Shaon Acharjee <shaon054@gmail.com>
2018-10-31 19:58:45 +0600

    New Java File

---
+++ CU.java
@@ -0,0 +1,15 @@
+package com.gdm.bduvguide.GeneralUniversity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gdm.bduvguide.R;
+
+public class CU extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cu);
+    }
+}
